0e34ebe390315c11064e36d81550676e2082ac4c
Ismael Hermoso Rivas <ismael.hermoso.rivas@hotmail.com>
2023-04-13 19:07:31 +0200

    Documentacion jsdoc

--- geomir23/src/store.js
+++ geomir23/src/store.js
@@ -7,7 +7,25 @@ import commentSlice from './slices/comments/commentSlice'
 import placeSlice from './slices/places/placeSlice'
 import postSlice from './slices/posts/postSlice'
 
+/**
+ * The Redux store instance.
+ * @type {Store}
+ */
+
 export const store = configureStore({
+
+/**
+ * Redux store instance configured with multiple reducers.
+ * @typedef {Object} Store
+ * @property {Object} Store.todos - The todos reducer.
+ * @property {Object} Store.placeMarks - The place marks reducer.
+ * @property {Object} Store.postMarks - The post marks reducer.
+ * @property {Object} Store.reviews - The reviews reducer.
+ * @property {Object} Store.comments - The comments reducer.
+ * @property {Object} Store.places - The places reducer.
+ * @property {Object} Store.posts - The posts reducer.
+ */
+
   reducer: {
     todos: todosReducer,
     placeMarks: placesMarksReducer,
